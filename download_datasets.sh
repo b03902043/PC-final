210a1a4a7643831284b7ed5644aec225c9bfb102
邱綜樹 <b03902043@HC-TITANX.local>
2017-06-12 19:46:01 +0800

    update download shell file

--- download_datasets.sh
+++ download_datasets.sh
@@ -9,6 +9,7 @@ URL=https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/$FILE.zip
 ZIP_FILE=./input/$FILE.zip
 TARGET_DIR=./input/$FILE/
 wget -N $URL -O $ZIP_FILE
+mkdir ./input
 mkdir $TARGET_DIR
 unzip $ZIP_FILE -d ./input/
 rm $ZIP_FILE
