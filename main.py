84a0f1f90850bf0a7247e2d133e930d453ca4ce2
treetree <b03902043@ntu.edu.tw>
2017-06-16 01:02:43 +0800

    bch image cannot too large or will cause std:alloc  error

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def make_trainable(net, val):
 
 output_folder = 'output'
 
-cycle_gan = CycleGAN(num_features=64, shape=(128, 128, 3), bch_img_num=100, task_name='summer2winter_yosemite')
+cycle_gan = CycleGAN(num_features=64, shape=(128, 128, 3), bch_img_num=20, task_name='summer2winter_yosemite')
 try:
 	cycle_gan.fit(epoch_num=200, disc_iter=20, save_period=1, pic_dir=output_folder)
 	cycle_gan.save(path=output_folder)
